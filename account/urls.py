02a376c1c650d95031645c69f4bcb86d5e734164
Roman <yummison44@gmail.com>
2025-06-15 20:14:07 +0200

    Registration, logout, change password, reset password, extending base User model by class Profile

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
+from django.contrib.auth import views as auth_views
 
-
+# path('login/', user_login, name='login')
 urlpatterns = [
-    path('login/', user_login, name='login'),
+    path('', dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', register, name='register'),
+    path('edit/', edit, name='edit'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from .forms import LoginForm
+from django.shortcuts import render, redirect
+from .forms import LoginForm, UserRegistrationForm
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+from .forms import UserEditForm, ProfileEditForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -23,3 +26,42 @@ def user_login(request):
     return render(request, 'account/login.html', {'form': form})
 
 
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+
+
+def logged_out(request):
+    return render(request, 'registration/logged_out.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            Profile.objects.create(user=new_user)
+            return render(request, 'account/register_done.html', {'new_user': new_user})
+        else:
+            return render(request, 'account/register.html', {'user_form': user_form})
+    else:
+        user_form = UserRegistrationForm()
+        return render(request, 'account/register.html', {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_edit_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_edit_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_edit_form.is_valid() and profile_edit_form.is_valid():
+            user_edit_form.save()
+            profile_edit_form.save()
+            return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    else:
+        user_edit_form = UserEditForm(instance=request.user)
+        profile_edit_form = ProfileEditForm(instance=request.user.profile)
+
+    return render(request, 'account/edit.html', context={'user_edit_form': user_edit_form, 'profile_edit_form': profile_edit_form})
